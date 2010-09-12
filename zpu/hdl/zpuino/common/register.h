94d73268b37aa73b64d60a59ad1ade0da24ed2e1
Alvaro <alvieboy@alvie.com>
2010-09-12 12:27:16 +0100

    register: update registers for new SPI flags and new timer flags.

--- zpu/hdl/zpuino/common/register.h
+++ zpu/hdl/zpuino/common/register.h
@@ -30,29 +30,33 @@
 #define TMR0CNT  REGISTER(TIMERSBASE,1)
 #define TMR0CMP  REGISTER(TIMERSBASE,2)
 #define TIMERTSC REGISTER(TIMERSBASE,3)
+#define TMR0OCR  REGISTER(TIMERSBASE,3) /* Same as TSC */
 #define TMR1CTL  REGISTER(TIMERSBASE,4)
 #define TMR1CNT  REGISTER(TIMERSBASE,5)
 #define TMR1CMP  REGISTER(TIMERSBASE,6)
-
+#define TMR1OCR  REGISTER(TIMERSBASE,7)
 
 #define INTRCTL  REGISTER(INTRBASE,0)
 
 /* Timer CTL bits */
 
-#define TCTLENA 0
-#define TCTLCCM 1
-#define TCTLDIR 2
-#define TCTLIEN 3
-#define TCTLCP0 4
-#define TCTLCP1 5
-#define TCTLCP2 6
-#define TCTLIF  7
-/* SPI bits */
+#define TCTLENA 0 /* Timer Enable */
+#define TCTLCCM 1 /* Clear on Compare Match */
+#define TCTLDIR 2 /* Direction */
+#define TCTLIEN 3 /* Interrupt enable */
+#define TCTLCP0 4 /* Clock prescaler bit 0 */
+#define TCTLCP1 5 /* Clock prescaler bit 1 */
+#define TCTLCP2 6 /* Clock prescaler bit 2 */
+#define TCTLIF  7 /* Interrupt flag */
+#define TCTLOCE 8 /* Output compare enable */
 
-#define SPICP0   1
-#define SPICP1   2
-#define SPICPOL  3
-#define SPIREADY  0
+/* SPI bits */
+#define SPIREADY 0 /* SPI ready */
+#define SPICP0   1 /* Clock prescaler bit 0 */
+#define SPICP1   2 /* Clock prescaler bit 1 */
+#define SPICPOL  3 /* Clock polarity */
+#define SPISRE   4 /* Sample on Rising Edge */
+
 /* Baud rate computation */
 
 #define BAUDRATEGEN(x) ((CLK_FREQ/(x))/4)
