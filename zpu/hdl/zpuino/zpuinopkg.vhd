5a8ff64a3e90d3bdd386637c3c6a81e5444b2299
Alvaro <alvieboy@alvie.com>
2010-09-24 18:09:17 +0100

    crc16: add crc16 module definition

--- zpu/hdl/zpuino/zpuinopkg.vhd
+++ zpu/hdl/zpuino/zpuinopkg.vhd
@@ -181,4 +181,17 @@ package zpuinopkg is
   );
   end component zpuino_sigmadelta;
 
+  component zpuino_crc16 is
+  port (
+    clk:      in std_logic;
+	 	areset:   in std_logic;
+    read:     out std_logic_vector(wordSize-1 downto 0);
+    write:    in std_logic_vector(wordSize-1 downto 0);
+    address:  in std_logic_vector(1 downto 0);
+    we:       in std_logic;
+    re:       in std_logic;
+    busy:     out std_logic
+  );
+  end component zpuino_crc16;
+
 end package zpuinopkg;
