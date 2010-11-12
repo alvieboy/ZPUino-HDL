bc47d546053c052ac276b5ad54cdb26272fd64d5
Alvaro <alvieboy@alvie.com>
2010-11-12 20:32:29 +0000

    zpupkg: fix memory size

--- zpu/hdl/zpuino/zpupkg.vhd
+++ zpu/hdl/zpuino/zpupkg.vhd
@@ -61,11 +61,11 @@ package zpupkg is
 	component dualport_ram is
 	port (clk : in std_logic;
 		memAWriteEnable : in std_logic;
-		memAAddr : in std_logic_vector(maxAddrBitBRAM downto minAddrBit);
+		memAAddr : in std_logic_vector(maxAddrBit downto minAddrBit);
 		memAWrite : in std_logic_vector(wordSize-1 downto 0);
 		memARead : out std_logic_vector(wordSize-1 downto 0);
 		memBWriteEnable : in std_logic;
-		memBAddr : in std_logic_vector(maxAddrBitBRAM downto minAddrBit);
+		memBAddr : in std_logic_vector(maxAddrBit downto minAddrBit);
 		memBWrite : in std_logic_vector(wordSize-1 downto 0);
 		memBRead : out std_logic_vector(wordSize-1 downto 0);
     memErr: out std_logic
