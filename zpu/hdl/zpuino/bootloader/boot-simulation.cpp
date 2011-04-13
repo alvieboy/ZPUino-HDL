4d7af367cd2849888669d7a2619a19a88584fb56
Alvaro <alvieboy@alvie.com>
2011-04-13 20:17:26 +0100

    sim: fix bootloader, add test for interrupts

--- zpu/hdl/zpuino/bootloader/boot-simulation.cpp
+++ zpu/hdl/zpuino/bootloader/boot-simulation.cpp
@@ -14,14 +14,15 @@ unsigned int _memreg[4];
 
 extern "C" void (*ivector)(void);
 
+
 void spi_disable()
 {
-	digitalWriteS<SPI_FLASH_SEL_PIN,HIGH>::apply();
+	digitalWrite(SPI_FLASH_SEL_PIN,HIGH);
 }
 
 static inline void spi_enable()
 {
-	digitalWriteS<SPI_FLASH_SEL_PIN,LOW>::apply();
+	digitalWrite(SPI_FLASH_SEL_PIN,LOW);
 }
 
 static inline void spiwrite(unsigned int i)
@@ -63,6 +64,7 @@ extern "C" void __attribute__((noreturn)) spi_copy_impl()
 
 extern "C" void _zpu_interrupt()
 {
+	TMR0CTL &= ~(BIT(TCTLIF));
 }
 
 void configure_pins()
@@ -78,11 +80,11 @@ void configure_pins()
 
 	GPIOPPSMODE(0) = BIT(4)|BIT(3);
 
-	pinModeS<IOPIN_SPI_MOSI,OUTPUT>::apply();
-	pinModeS<IOPIN_SPI_SCK,OUTPUT>::apply();
-	pinModeS<IOPIN_SPI_MISO,INPUT>::apply();
+	pinMode(IOPIN_SPI_MOSI,OUTPUT);
+	pinMode(IOPIN_SPI_SCK,OUTPUT);
+	pinMode(IOPIN_SPI_MISO,INPUT);
 
-	pinModeS<FPGA_SS_B,OUTPUT>::apply();
+	pinMode(FPGA_SS_B,OUTPUT);
 
 	spi_disable();
 	spi_enable();
@@ -101,15 +103,24 @@ void configure_pins()
 
 extern "C" int main(int argc,char**argv)
 {
-//	ivector = &_zpu_interrupt;
+	ivector = &_zpu_interrupt;
 	SPICTL=BIT(SPICPOL)|BIT(SPICP0)|BIT(SPISRE)|BIT(SPIEN)|BIT(SPIBLOCK);
 
 	configure_pins();
 
+	TMR0CMP = (CLK_FREQ/100000U)-1;
+	TMR0CNT = 0x0;
+	TMR0CTL = BIT(TCTLENA)|BIT(TCTLCCM)|BIT(TCTLDIR)|BIT(TCTLCP0)|BIT(TCTLIEN);
+
 	UARTCTL = BAUDRATEGEN(1000000) | BIT(UARTEN);
+
+	INTRMASK = BIT(INTRLINE_TIMER0);
 	INTRCTL=1;
 	CRC16POLY = 0x8408; // CRC16-CCITT
 	SPICTL=BIT(SPICPOL)|BIT(SPICP0)|BIT(SPISRE)|BIT(SPIEN);
-
-	spi_copy();
+	/*
+	 spi_copy();
+	 */
+	while (1) {
+	}
 }
