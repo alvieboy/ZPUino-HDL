8e46a504dc1eaeeda74d8aab7741f14da3aef1f5
Alvaro <alvieboy@alvie.com>
2011-08-12 12:35:23 +0100

    simulation: add pty pseudo implementation

---
+++ zpu/hdl/zpuino/pty-impl.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int pty_initialize()
+{
+	return 0;
+}
+
+int pty_available()
+{
+	return -1;
+}
+
+int pty_receive()
+{
+	return 0;
+}
+
+int pty_transmit(int t)
+{
+	return 0;
+}
