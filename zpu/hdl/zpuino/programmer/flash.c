c048d26c314af2fbbdcd99fb54097cdf43354db4
Alvaro <alvieboy@alvie.com>
2012-03-13 09:21:40 +0000

    programmer: add M25L6445E flash

--- zpu/hdl/zpuino/programmer/flash.c
+++ zpu/hdl/zpuino/programmer/flash.c
@@ -40,7 +40,6 @@ extern flash_driver_t sst25vf_flash;
 extern flash_driver_t atmel_flash;
 
 
-
 flash_info_t flash_list[] =
 {
 	/* Dummy flash driver, for direct upload */
@@ -48,5 +47,6 @@ flash_info_t flash_list[] =
 	{ 0x20, 0x20, 0x15, 256, 65536, 32, "M25P16", &m25p_flash },
 	{ 0xBF, 0x25, 0x8D, 256, 4096, 128, "SST25VF040B", &sst25vf_flash },
 	{ 0x1F, 0x25, 0x00, 264, 2112, 512, "AT45DB081D", &atmel_flash }, /* Note: we use blocks here, not sectors */
+	{ 0xC2, 0x20, 0x17, 256, 65536, 128, "MX25L6445E", &m25p_flash },
 	{ 0, 0, 0, 0, 0, 0, NULL, NULL }
 };
