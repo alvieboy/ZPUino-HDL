88118090b0436ec4952d417ad44882ab3756102f
Alvaro <alvieboy@alvie.com>
2012-06-21 08:57:49 +0100

    bootloader: ensure SPI TX ends before deselecting

--- zpu/hdl/zpuino/bootloader/boot.cpp
+++ zpu/hdl/zpuino/bootloader/boot.cpp
@@ -99,7 +99,7 @@ extern "C" void printhex(unsigned int c)
 	printhexbyte(c);
 }
 
-void spi_copy();
+static void spi_copy();
 
 #ifdef DEBUG_SERIAL
 const unsigned char serialbuffer[] = {
@@ -199,8 +199,9 @@ static void outbyte(int c)
 	UARTDATA=c;
 }
 
-static void spi_disable()
+static void spi_disable(register_t base)
 {
+	(void)*base; // Let SPI finish
 	digitalWrite(SPI_FLASH_SEL_PIN,HIGH);
 }
 
@@ -209,11 +210,11 @@ static void spi_enable()
 	digitalWrite(SPI_FLASH_SEL_PIN,LOW);
 }
 
-static void spi_reset()
+static void spi_reset(register_t base)
 {
-	spi_disable();
+	spi_disable(base);
 	spi_enable();
-	spi_disable();
+	spi_disable(base);
 }
 
 static inline void waitspiready()
@@ -247,7 +248,7 @@ static inline unsigned int spiread(register_t base)
 	return *base;
 }
 
-void spi_copy()
+static void spi_copy()
 {
 	// Make sure we are on top of stack. We can safely discard everything
 	__asm__("im %0\n"
@@ -275,15 +276,8 @@ extern "C" void start()
 unsigned start_read_size(register_t spidata)
 {
 	spiwrite(spidata,0x0B);
-	spiwrite(spidata,SPIOFFSET >> 16);
-	spiwrite(spidata,SPIOFFSET >> 8);
-	spiwrite(spidata,SPIOFFSET);
-	spiwrite(spidata,0);
-
-	// Read size.
-
-	spiwrite(spidata,0);
-	spiwrite(spidata,0);
+	spiwrite(spidata+4,SPIOFFSET);
+	spiwrite(spidata+4,0);
 	return spiread(spidata) & 0xffff;
 }
 
@@ -344,10 +338,10 @@ extern "C" void __attribute__((noreturn)) spi_copy_impl()
    // printstring("Filled\n");
 #endif
 
-	spi_disable();
+	spi_disable(spidata);
 
 	if (sketchcrc != REGISTER(crc16base,ROFF_CRC16ACC)) {
-        outbyte('C');
+		outbyte('C');
         //printstring("CRC");
 //		printstring("CRC error, please reset\r\n");
 		/*
@@ -418,7 +412,7 @@ static int spi_read_status(register_t spidata)
 
 	spiwrite(spidata,0x00);
 	status =  spiread(spidata) & 0xff;
-	spi_disable();
+	spi_disable(spidata);
 	return status;
 }
 
@@ -427,12 +421,15 @@ static unsigned int spi_read_id(register_t spidata)
 	unsigned int ret;
 
 	spi_enable();
+    /*
 	spiwrite(spidata,0x9F);
 	spiwrite(spidata,0x00);
 	spiwrite(spidata,0x00);
 	spiwrite(spidata,0x00);
+	*/
+	spiwrite(spidata+6, 0x9f000000);
 	ret = spiread(spidata);
-	spi_disable();
+	spi_disable(spidata);
 	return ret;
 }
 
@@ -495,7 +492,7 @@ static void cmd_raw_send_receive(register_t spidata,unsigned char *buffer)
 		spiwrite(spidata,0x00);
 		buffer[count] = spiread(spidata);
 	}
-	spi_disable();
+	spi_disable(spidata);
 
 	// Send back
 	prepareSend();
@@ -523,7 +520,7 @@ static void cmd_sst_aai_program(register_t spidata,unsigned char *buffer)
 	// Enable writes
 	spi_enable();
 	spiwrite(spidata,0x06);
-	spi_disable();
+	spi_disable(spidata);
 
 	spi_enable();
 	spiwrite(spidata,0xAD);
@@ -543,7 +540,7 @@ static void cmd_sst_aai_program(register_t spidata,unsigned char *buffer)
 		}
 		spiwrite(spidata,buffer[6+count]);
 		spiwrite(spidata,buffer[6+count+1]);
-		spi_disable();
+		spi_disable(spidata);
 		// Read back status, wait for completion
 		while (spi_read_status(spidata) & 1);
 	}
@@ -552,7 +549,7 @@ static void cmd_sst_aai_program(register_t spidata,unsigned char *buffer)
 
 	spi_enable();
 	spiwrite(spidata,0x04);
-	spi_disable();
+	spi_disable(spidata);
 	// Send back
 	prepareSend();
 	sendByte(REPLY(BOOTLOADER_CMD_SSTAAIPROGRAM));
@@ -674,7 +671,7 @@ void cmd_start(register_t spidata, unsigned char *buffer)
 
 	spi_enable();
 	bdata.spiend = (start_read_size(spidata)<<2) + SPIOFFSET + 4;
-	spi_disable();
+	spi_disable(spidata);
 	flush();
 	start();
 }
@@ -814,11 +811,11 @@ extern "C" int main(int argc,char**argv)
 	CRC16POLY = 0x8408; // CRC16-CCITT
 	SPICTL=BIT(SPICPOL)|BOARD_SPI_DIVIDER|BIT(SPISRE)|BIT(SPIEN)|BIT(SPIBLOCK);
 	// Reset flash
-	spi_reset();
+	spi_reset(&SPIDATA);
 #ifdef __ZPUINO_PAPILIO_ONE__
 	spi_enable();
 	spiwrite(0x4); // Disable WREN for SST flash
-	spi_disable();
+	spi_disable(&SPIDATA);
 #endif
 
 	syncSeen = 0;
