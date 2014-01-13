1816259a0318f3c2268b11280c9fe8ca2aef473f
Alvaro <alvieboy@alvie.com>
2014-01-13 19:51:51 +0000

    sync.

--- zpu/hdl/zpuino/boards/papilio-pro/S6LX9/variants/logicstart/papilio_pro_top.vhd
+++ zpu/hdl/zpuino/boards/papilio-pro/S6LX9/variants/logicstart/papilio_pro_top.vhd
@@ -57,8 +57,32 @@ entity papilio_pro_top is
 
     -- WING connections
     WING_A:     inout std_logic_vector(15 downto 0);
-    WING_B:     inout std_logic_vector(15 downto 0);
-    WING_C:     inout std_logic_vector(15 downto 0);
+
+    HSYNC:      out std_logic;
+    VSYNC:      out std_logic;
+    RED:        out std_logic_vector(3 downto 0);
+    GREEN:      out std_logic_vector(3 downto 0);
+    BLUE:       out std_logic_vector(3 downto 0);
+    AUDIO:      out std_logic_vector(1 downto 0);
+
+    PS2CLK2:    inout std_logic;
+    PS2CLK1:    inout std_logic;
+    PS2DAT2:    inout std_logic;
+    PS2DAT1:    inout std_logic;
+
+    RESET:      in std_logic;
+    JOY_FIRE1:  in std_logic;
+    JOY_FIRE2:  in std_logic;
+    JOY_UP:     in std_logic;
+    JOY_DOWN:   in std_logic;
+    JOY_LEFT:   in std_logic;
+    JOY_RIGHT:  in std_logic;
+    JOY_SEL:    in std_logic;
+
+    SD_NCS:     out std_logic;
+    SD_DI:      out std_logic;
+    SD_DO:      in std_logic;
+    SD_SCK:     out std_logic;
 
     -- UART (FTDI) connection
     TXD:        out std_logic;
@@ -99,6 +123,7 @@ architecture behave of papilio_pro_top is
     clkout: out std_logic;
     clkout1: out std_logic;
     clkout2: out std_logic;
+    clkvga: out  std_logic;
     rstout: out std_logic
   );
   end component;
@@ -167,7 +192,7 @@ architecture behave of papilio_pro_top is
   signal slot_address:slot_address_type;
   signal slot_ack:    slot_std_logic_type;
   signal slot_interrupt: slot_std_logic_type;
-  signal slot_id:    slot_id_type;
+  signal slot_ids:    slot_id_type;
 
   -- 2nd SPI signals
   signal spi2_mosi:   std_logic;
@@ -252,6 +277,18 @@ architecture behave of papilio_pro_top is
   signal prom_rom_wb_cti_i:       std_logic_vector(2 downto 0);
   signal prom_rom_wb_stall_o:     std_logic;
 
+
+
+  signal m_wb_ack_o:       std_logic;
+  signal m_wb_dat_o:       std_logic_vector(wordSize-1 downto 0);
+  signal m_wb_dat_i:       std_logic_vector(wordSize-1 downto 0);
+  signal m_wb_adr_i:       std_logic_vector(maxAddrBitIncIO downto 0);
+  signal m_wb_cyc_i:       std_logic;
+  signal m_wb_stb_i:       std_logic;
+  signal m_wb_we_i:        std_logic;
+  signal m_wb_cti_i:       std_logic_vector(2 downto 0);
+  signal m_wb_stall_o:     std_logic;
+
   signal memory_enable: std_logic;
 
   component sdram_ctrl is
@@ -326,6 +363,49 @@ architecture behave of papilio_pro_top is
   signal uart2_rx: std_logic;
   signal uart2_tx: std_logic;
 
+  component vga_640_480 is
+  port(
+    wb_clk_i: in std_logic;
+	 	wb_rst_i: in std_logic;
+    wb_dat_o: out std_logic_vector(wordSize-1 downto 0);
+    wb_dat_i: in std_logic_vector(wordSize-1 downto 0);
+    wb_adr_i: in std_logic_vector(maxIObit downto minIObit);
+    wb_we_i:  in std_logic;
+    wb_cyc_i: in std_logic;
+    wb_stb_i: in std_logic;
+    wb_ack_o: out std_logic;
+    id:       out slot_id;
+
+    -- Wishbone MASTER interface
+    mi_wb_dat_i: in std_logic_vector(wordSize-1 downto 0);
+    mi_wb_dat_o: out std_logic_vector(wordSize-1 downto 0);
+    mi_wb_adr_o: out std_logic_vector(maxAddrBitIncIO downto 0);
+    mi_wb_sel_o: out std_logic_vector(3 downto 0);
+    mi_wb_cti_o: out std_logic_vector(2 downto 0);
+    mi_wb_we_o:  out std_logic;
+    mi_wb_cyc_o: out std_logic;
+    mi_wb_stb_o: out std_logic;
+    mi_wb_ack_i: in std_logic;
+    mi_wb_stall_i: in std_logic;
+
+    -- VGA signals
+    vgaclk:     in std_logic;
+    vga_hsync:  out std_logic;
+    vga_vsync:  out std_logic;
+    vga_b:      out std_logic_vector(4 downto 0);
+    vga_r:      out std_logic_vector(4 downto 0);
+    vga_g:      out std_logic_vector(4 downto 0)
+  );
+  end component;
+
+
+  signal vgaclk: std_logic;
+  signal vga_hsync:   std_logic;
+  signal vga_vsync:   std_logic;
+  signal vga_b:       std_logic_vector(4 downto 0);
+  signal vga_r:       std_logic_vector(4 downto 0);
+  signal vga_g:       std_logic_vector(4 downto 0);
+
 begin
 
   wb_clk_i <= sysclk;
@@ -349,6 +429,7 @@ begin
     clkout  => sysclk,
     clkout1  => sysclk_sram_we,
     clkout2  => sysclk_sram_wen,
+    clkvga =>  vgaclk,
     rstout  => clkgen_rst
   );
 
@@ -368,39 +449,15 @@ begin
   pin13: IOPAD port map(I => gpio_o(13),O => gpio_i(13),T => gpio_t(13),C => sysclk,PAD => WING_A(13) );
   pin14: IOPAD port map(I => gpio_o(14),O => gpio_i(14),T => gpio_t(14),C => sysclk,PAD => WING_A(14) );
   pin15: IOPAD port map(I => gpio_o(15),O => gpio_i(15),T => gpio_t(15),C => sysclk,PAD => WING_A(15) );
-  pin16: IOPAD port map(I => gpio_o(16),O => gpio_i(16),T => gpio_t(16),C => sysclk,PAD => WING_B(0) );
-  pin17: IOPAD port map(I => gpio_o(17),O => gpio_i(17),T => gpio_t(17),C => sysclk,PAD => WING_B(1) );
-  pin18: IOPAD port map(I => gpio_o(18),O => gpio_i(18),T => gpio_t(18),C => sysclk,PAD => WING_B(2) );
-  pin19: IOPAD port map(I => gpio_o(19),O => gpio_i(19),T => gpio_t(19),C => sysclk,PAD => WING_B(3) );
-  pin20: IOPAD port map(I => gpio_o(20),O => gpio_i(20),T => gpio_t(20),C => sysclk,PAD => WING_B(4) );
-  pin21: IOPAD port map(I => gpio_o(21),O => gpio_i(21),T => gpio_t(21),C => sysclk,PAD => WING_B(5) );
-  pin22: IOPAD port map(I => gpio_o(22),O => gpio_i(22),T => gpio_t(22),C => sysclk,PAD => WING_B(6) );
-  pin23: IOPAD port map(I => gpio_o(23),O => gpio_i(23),T => gpio_t(23),C => sysclk,PAD => WING_B(7) );
-  pin24: IOPAD port map(I => gpio_o(24),O => gpio_i(24),T => gpio_t(24),C => sysclk,PAD => WING_B(8) );
-  pin25: IOPAD port map(I => gpio_o(25),O => gpio_i(25),T => gpio_t(25),C => sysclk,PAD => WING_B(9) );
-  pin26: IOPAD port map(I => gpio_o(26),O => gpio_i(26),T => gpio_t(26),C => sysclk,PAD => WING_B(10) );
-  pin27: IOPAD port map(I => gpio_o(27),O => gpio_i(27),T => gpio_t(27),C => sysclk,PAD => WING_B(11) );
-  pin28: IOPAD port map(I => gpio_o(28),O => gpio_i(28),T => gpio_t(28),C => sysclk,PAD => WING_B(12) );
-  pin29: IOPAD port map(I => gpio_o(29),O => gpio_i(29),T => gpio_t(29),C => sysclk,PAD => WING_B(13) );
-  pin30: IOPAD port map(I => gpio_o(30),O => gpio_i(30),T => gpio_t(30),C => sysclk,PAD => WING_B(14) );
-  pin31: IOPAD port map(I => gpio_o(31),O => gpio_i(31),T => gpio_t(31),C => sysclk,PAD => WING_B(15) );
-  pin32: IOPAD port map(I => gpio_o(32),O => gpio_i(32),T => gpio_t(32),C => sysclk,PAD => WING_C(0) );
-  pin33: IOPAD port map(I => gpio_o(33),O => gpio_i(33),T => gpio_t(33),C => sysclk,PAD => WING_C(1) );
-  pin34: IOPAD port map(I => gpio_o(34),O => gpio_i(34),T => gpio_t(34),C => sysclk,PAD => WING_C(2) );
-  pin35: IOPAD port map(I => gpio_o(35),O => gpio_i(35),T => gpio_t(35),C => sysclk,PAD => WING_C(3) );
-  pin36: IOPAD port map(I => gpio_o(36),O => gpio_i(36),T => gpio_t(36),C => sysclk,PAD => WING_C(4) );
-  pin37: IOPAD port map(I => gpio_o(37),O => gpio_i(37),T => gpio_t(37),C => sysclk,PAD => WING_C(5) );
-  pin38: IOPAD port map(I => gpio_o(38),O => gpio_i(38),T => gpio_t(38),C => sysclk,PAD => WING_C(6) );
-  pin39: IOPAD port map(I => gpio_o(39),O => gpio_i(39),T => gpio_t(39),C => sysclk,PAD => WING_C(7) );
-  pin40: IOPAD port map(I => gpio_o(40),O => gpio_i(40),T => gpio_t(40),C => sysclk,PAD => WING_C(8) );
-  pin41: IOPAD port map(I => gpio_o(41),O => gpio_i(41),T => gpio_t(41),C => sysclk,PAD => WING_C(9) );
-  pin42: IOPAD port map(I => gpio_o(42),O => gpio_i(42),T => gpio_t(42),C => sysclk,PAD => WING_C(10) );
-  pin43: IOPAD port map(I => gpio_o(43),O => gpio_i(43),T => gpio_t(43),C => sysclk,PAD => WING_C(11) );
-  pin44: IOPAD port map(I => gpio_o(44),O => gpio_i(44),T => gpio_t(44),C => sysclk,PAD => WING_C(12) );
-  pin45: IOPAD port map(I => gpio_o(45),O => gpio_i(45),T => gpio_t(45),C => sysclk,PAD => WING_C(13) );
-  pin46: IOPAD port map(I => gpio_o(46),O => gpio_i(46),T => gpio_t(46),C => sysclk,PAD => WING_C(14) );
-  pin47: IOPAD port map(I => gpio_o(47),O => gpio_i(47),T => gpio_t(47),C => sysclk,PAD => WING_C(15) );
 
+  pin36: OPAD port map(I => gpio_o(36),O => gpio_i(36), PAD => SD_NCS );
+
+
+  AUDIO(0) <= sigmadelta_spp_data(0);
+  AUDIO(1) <= sigmadelta_spp_data(1);
+
+  SD_SCK <= '0';
+  SD_DI <= '0';
 
   -- Other ports are special, we need to avoid outputs on input-only pins
 
@@ -426,7 +483,7 @@ begin
       slot_address  => slot_address,
       slot_ack      => slot_ack,
       slot_interrupt=> slot_interrupt,
-      slot_id       => slot_id,
+      slot_id       => slot_ids,
 
       pps_in_slot   => ppsin_info_slot,
       pps_in_pin    => ppsin_info_pin,
@@ -434,14 +491,14 @@ begin
       pps_out_slot => ppsout_info_slot,
       pps_out_pin  => ppsout_info_pin,
 
-      m_wb_dat_o    => open,
-      m_wb_dat_i    => (others => 'X'),
-      m_wb_adr_i    => (others => 'X'),
-      m_wb_we_i     => '0',
-      m_wb_cyc_i    => '0',
-      m_wb_stb_i    => '0',
-      m_wb_ack_o    => open,
-      m_wb_stall_o  => open,
+      m_wb_dat_o    => m_wb_dat_o,
+      m_wb_dat_i    => m_wb_dat_i,
+      m_wb_adr_i    => m_wb_adr_i,
+      m_wb_we_i     => m_wb_we_i,
+      m_wb_cyc_i    => m_wb_cyc_i,
+      m_wb_stb_i    => m_wb_stb_i,
+      m_wb_ack_o    => m_wb_ack_o,
+      m_wb_stall_o  => m_wb_stall_o,
 
       wb_ack_i      => sram_wb_ack_o,
       wb_stall_i    => sram_wb_stall_o,
@@ -475,7 +532,7 @@ begin
     wb_stb_i      => slot_stb(1),
     wb_ack_o      => slot_ack(1),
     wb_inta_o     => slot_interrupt(1),
-    id            => slot_id(1),
+    id            => slot_ids(1),
 
     enabled       => open,
     tx            => tx,
@@ -501,7 +558,7 @@ begin
     wb_stb_i      => slot_stb(2),
     wb_ack_o      => slot_ack(2),
     wb_inta_o     => slot_interrupt(2),
-    id            => slot_id(2),
+    id            => slot_ids(2),
 
     spp_data      => gpio_spp_data,
     spp_read      => gpio_spp_read,
@@ -540,7 +597,7 @@ begin
     wb_cyc_i      => slot_cyc(3),
     wb_stb_i      => slot_stb(3),
     wb_ack_o      => slot_ack(3),
-    id            => slot_id(3),
+    id            => slot_ids(3),
 
     wb_inta_o     => slot_interrupt(3), -- We use two interrupt lines
     wb_intb_o     => slot_interrupt(4), -- so we borrow intr line from slot 4
@@ -568,7 +625,7 @@ begin
     wb_stb_i      => slot_stb(4),
     wb_ack_o      => slot_ack(4),
     -- wb_inta_o     => slot_interrupt(4), -- Used by the Timers.
-    id            => slot_id(4),
+    id            => slot_ids(4),
 
     mosi          => spi_pf_mosi,
     miso          => spi_pf_miso,
@@ -592,7 +649,7 @@ begin
     wb_stb_i      => slot_stb(5),
     wb_ack_o      => slot_ack(5),
     wb_inta_o     => slot_interrupt(5),
-    id            => slot_id(5),
+    id            => slot_ids(5),
 
     spp_data      => sigmadelta_spp_data,
     spp_en        => open,
@@ -615,7 +672,7 @@ begin
     wb_stb_i      => slot_stb(6),
     wb_ack_o      => slot_ack(6),
     wb_inta_o     => slot_interrupt(6),
-    id            => slot_id(6),
+    id            => slot_ids(6),
 
     mosi          => spi2_mosi,
     miso          => spi2_miso,
@@ -641,7 +698,7 @@ begin
     wb_stb_i      => slot_stb(7),
     wb_ack_o      => slot_ack(7),
     wb_inta_o     => slot_interrupt(7),
-    id            => slot_id(7)
+    id            => slot_ids(7)
   );
 
   --
@@ -660,7 +717,7 @@ begin
     wb_stb_i      => slot_stb(8),
     wb_ack_o      => slot_ack(8),
     wb_inta_o     => slot_interrupt(8),
-    id            => slot_id(8),
+    id            => slot_ids(8),
     tx            => uart2_tx,
     rx            => uart2_rx
   );
@@ -710,7 +767,7 @@ begin
     wb_stb_i      => slot_stb(9),
     wb_ack_o      => slot_ack(9),
     wb_inta_o     => slot_interrupt(9),
-    id            => slot_id(9)
+    id            => slot_ids(9)
   );
 
 
@@ -730,7 +787,7 @@ begin
     wb_stb_i      => slot_stb(10),
     wb_ack_o      => slot_ack(10),
     wb_inta_o     => slot_interrupt(10),
-    id            => slot_id(10)
+    id            => slot_ids(10)
   );
 
   --
@@ -749,7 +806,7 @@ begin
     wb_stb_i      => slot_stb(11),
     wb_ack_o      => slot_ack(11),
     wb_inta_o     => slot_interrupt(11),
-    id            => slot_id(11)
+    id            => slot_ids(11)
   );
 
   --
@@ -768,31 +825,47 @@ begin
     wb_stb_i      => slot_stb(12),
     wb_ack_o      => slot_ack(12),
     wb_inta_o     => slot_interrupt(12),
-    id            => slot_id(12)
+    id            => slot_ids(12)
   );
 
-  --
-  -- IO SLOT 13
-  --
+  vga: vga_640_480
+    port map (
+    wb_clk_i    => wb_clk_i,
+	 	wb_rst_i    => wb_rst_i,
+    wb_dat_o    => slot_read(13),
+    wb_dat_i    => slot_write(13),
+    wb_adr_i    => slot_address(13),
+    wb_we_i     => slot_we(13),
+    wb_cyc_i    => slot_cyc(13),
+    wb_stb_i    => slot_stb(13),
+    wb_ack_o    => slot_ack(13),
+    id          => slot_ids(13),
 
-  slot13: zpuino_empty_device
-  port map (
-    wb_clk_i      => wb_clk_i,
-	 	wb_rst_i      => wb_rst_i,
-    wb_dat_o      => slot_read(13),
-    wb_dat_i      => slot_write(13),
-    wb_adr_i      => slot_address(13),
-    wb_we_i       => slot_we(13),
-    wb_cyc_i      => slot_cyc(13),
-    wb_stb_i      => slot_stb(13),
-    wb_ack_o      => slot_ack(13),
-    wb_inta_o     => slot_interrupt(13),
-    id            => slot_id(13)
+    -- Wishbone MASTER interface
+    mi_wb_dat_i   => m_wb_dat_o,
+    mi_wb_dat_o   => m_wb_dat_i,
+    mi_wb_adr_o   => m_wb_adr_i(maxAddrBitIncIO downto 0),
+    mi_wb_sel_o   => open,
+    mi_wb_cti_o   => open,
+    mi_wb_we_o    => m_wb_we_i,
+    mi_wb_cyc_o   => m_wb_cyc_i,
+    mi_wb_stb_o   => m_wb_stb_i,
+    mi_wb_ack_i   => m_wb_ack_o,
+    mi_wb_stall_i => m_wb_stall_o,
+
+    vgaclk          => vgaclk,
+    vga_hsync       => vga_hsync,
+    vga_vsync       => vga_vsync,
+    vga_b           => vga_b,
+    vga_r           => vga_r,
+    vga_g           => vga_g
   );
 
-  --
-  -- IO SLOT 14
-  --
+  HSYNC <= vga_hsync;
+  VSYNC <= vga_vsync;
+  RED <= vga_r(4 downto 1);
+  GREEN <= vga_g(4 downto 1);
+  BLUE <= vga_b(4 downto 1);
 
   slot14: zpuino_empty_device
   port map (
@@ -806,7 +879,7 @@ begin
     wb_stb_i      => slot_stb(14),
     wb_ack_o      => slot_ack(14),
     wb_inta_o     => slot_interrupt(14),
-    id            => slot_id(14)
+    id            => slot_ids(14)
   );
 
   --
