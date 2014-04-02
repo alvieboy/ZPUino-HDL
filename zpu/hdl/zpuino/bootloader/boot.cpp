4335706d6fb7a91a0c1d78c94c9da928fbc21d05
Alvaro <alvieboy@alvie.com>
2014-04-02 19:32:42 +0100

    revert leds optimization, it's causing boot failures

--- zpu/hdl/zpuino/bootloader/boot.cpp
+++ zpu/hdl/zpuino/bootloader/boot.cpp
@@ -82,8 +82,8 @@ const unsigned char vstring[] = {
 	BOARD_ID
 };
 
+
 static void outbyte(int);
-extern "C" void _zpu_interrupt();
 
 void flush()
 {
@@ -208,10 +208,6 @@ static void enableTimer()
 #endif
 	TMR0CNT = 0x0;
 	TMR0CTL = BIT(TCTLENA)|BIT(TCTLCCM)|BIT(TCTLDIR)|BIT(TCTLCP0)|BIT(TCTLIEN);
-	ivector = &_zpu_interrupt;
-
-	INTRCTL = 1;
-
 }
 
 
@@ -371,6 +367,7 @@ extern "C" void __attribute__((noreturn)) spi_copy_impl()
 #ifdef __ZPUINO_NEXYS3__
 		digitalWrite(FPGA_LED_3, HIGH);
 #endif
+
 		while(1) {};
 	}
 
@@ -383,6 +380,7 @@ extern "C" void __attribute__((noreturn)) spi_copy_impl()
 #ifdef __ZPUINO_NEXYS3__
 		digitalWrite(FPGA_LED_4, HIGH);
 #endif
+
 		while(1) {};
 	}
 
@@ -404,9 +402,11 @@ extern "C" void __attribute__((noreturn)) spi_copy_impl()
 	while (1) {}
 }
 
+
 extern "C" void _zpu_interrupt()
 {
 	milisseconds++;
+//	outbyte('I');
 	TMR0CTL &= ~(BIT(TCTLIF));
 }
 
@@ -541,6 +541,7 @@ static void cmd_sst_aai_program(unsigned char *buffer)
 	register_t spidata = &SPIDATA; // Ensure this stays in stack
 
 #ifdef __SST_FLASH__
+
 	// buffer[1-2] is number of TX bytes
     // buffer[3-5] is address to program
 	// buffer[6...] is data to transmit.
@@ -774,7 +775,7 @@ inline void configure_pins()
 	pinMode(FPGA_PIN_FLASHCS, OUTPUT);
 	pinModePPS(FPGA_PIN_SDCS,LOW);
         pinMode(FPGA_PIN_SDCS, OUTPUT);
-	digitalWrite(FPGA_PIN_SDCS, HIGH);
+        digitalWrite(FPGA_PIN_SDCS, HIGH);
 }
 #endif
 
@@ -851,6 +852,8 @@ extern "C" int main(int argc,char**argv)
 #ifdef VERBOSE_LOADER
 	printstring("\r\nZPUINO\r\n");
 #endif
+
+
 	enableTimer();
 
 	CRC16POLY = 0x8408; // CRC16-CCITT
