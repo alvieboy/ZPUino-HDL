0781d188c7f11599a4ec818be4856b8ba986f294
Alvaro <alvieboy@alvie.com>
2014-05-07 18:16:14 +0100

    Fix board ids

--- zpu/hdl/zpuino/programmer/boards.c
+++ zpu/hdl/zpuino/programmer/boards.c
@@ -15,15 +15,18 @@ static struct board_type boards[] = {
 	{ 0xA4020F00, "GadgetFactory Papilio One 250 (extra RAM)" },
 	{ 0xA4030E00, "GadgetFactory Papilio Plus LX4" },
 	{ 0xA4041700, "GadgetFactory Papilio Pro LX9" },
-	{ 0xB4041700, "GadgetFactory Papilio Pro LX9 (ZPUino 2.0)" },
 	{ 0x83010F00, "Spartan 3E Starter Kit S3E500" },
         { 0x83011A00, "Spartan 3E Starter Kit S3E500 w/64MB DDR" },
-        { 0x93011A00, "Spartan 3E Starter Kit S3E500 (ZPUino 2.0)" },
 	{ 0x83010E01, "Spartan 3E Starter Kit S3E500 w/ 8-bit HQVGA" },
 	{ 0x84010F00, "Nexys2 board with S3E1200" },
         { 0xA5010F00, "OHO GODIL board with S3E500" },
         { 0xA6011900, "Xess XuLA2 with S6 LX9" },
-        { 0xB6011900, "Xess XuLA2 with S6 LX9 (ZPUino 2.0)" },
+        /* 2.0 boards */
+        { 0x93011A00, "Spartan 3E Starter Kit S3E500 (ZPUino 2.0)" },
+        { 0xB4041700, "GadgetFactory Papilio Pro LX9 (ZPUino 2.0)" },
+        { 0xB6011900, "Xess Xula1 with S6 LX9 (ZPUino 2.0)" },
+        { 0xB8011A00, "EMS11-22 board S6 LX45 (ZPUino 2.0)" },
+        { 0xBA011A00, "Saanlima Pipistrello with S6 LX45 (ZPUino 2.0)" },
 	{ 0x0, NULL }
 };
 
