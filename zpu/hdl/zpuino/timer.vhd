d36d22c839fd6cea0485b50e5997c4b8fe350cdc
Alvaro <alvieboy@alvie.com>
2014-06-17 16:14:16 +0100

    timers: Clear interrupt on reset

--- zpu/hdl/zpuino/timer.vhd
+++ zpu/hdl/zpuino/timer.vhd
@@ -219,6 +219,7 @@ end generate;
         w.ccm := '0';
         w.dir := '0';
         w.ien := '0';
+        w.intr := '0';
         w.pres := (others => '0');
         w.presrst := '1';
         w.updp := UPDATE_ZERO_SYNC;
