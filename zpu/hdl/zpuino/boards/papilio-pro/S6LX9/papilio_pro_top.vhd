6503dcf36c3c0b9d8fccc6627ee70b702294de48
Alvaro <alvieboy@alvie.com>
2016-11-22 11:12:13 +0000

    Fix default PPS assignment

--- zpu/hdl/zpuino/boards/papilio-pro/S6LX9/papilio_pro_top.vhd
+++ zpu/hdl/zpuino/boards/papilio-pro/S6LX9/papilio_pro_top.vhd
@@ -177,9 +177,9 @@ architecture behave of papilio_pro_top is
   -- GPIO Periperal Pin Select
   signal gpio_spp_data: std_logic_vector(PPSCOUNT_OUT-1 downto 0);
   signal gpio_spp_read: std_logic_vector(PPSCOUNT_IN-1 downto 0);
-  signal ppsout_info_slot: ppsoutinfotype := (others => -1);
+  signal ppsout_info_slot: ppsoutinfotype := (others => 0);
   signal ppsout_info_pin:  ppsoutinfotype;
-  signal ppsin_info_slot: ppsininfotype := (others => -1);
+  signal ppsin_info_slot: ppsininfotype := (others => 0);
   signal ppsin_info_pin:  ppsininfotype;
 
   -- Timer connections
