7084e75b7d6cb7acefb520a5502a45ac75699470
Alvaro <alvieboy@alvie.com>
2018-04-06 17:07:35 +0100

    Fix GPIO, add aliases

--- zpu/hdl/zpuino/boards/terasic/de0-nano/de0_nano_top.vhd
+++ zpu/hdl/zpuino/boards/terasic/de0-nano/de0_nano_top.vhd
@@ -105,14 +105,12 @@ architecture behave of de0_nano_top is
   signal gpio_i:      std_logic_vector(zpuino_gpio_count-1 downto 0);
 
   constant spp_cap_in: std_logic_vector(zpuino_gpio_count-1 downto 0) :=
-    "0000000000000000000000000000" &
-    "1111111111111111111111111111111111" &
-    "0000000000000000000000000000000000";
+    "000000000000000000000000000000000000000000000000" &
+    "1111111111111111" & "1111111111111111" & "1111111111111111";
 
   constant spp_cap_out: std_logic_vector(zpuino_gpio_count-1 downto 0) :=
-    "0000000000000000000000000000" &
-    "1111111111111111111111111111111111" &
-    "0000000000000000000000000000000000";
+    "000000000000000000000000000000000000000000000000" &
+    "1111111111111111" & "1111111111111111" & "1111111111111111";
 
   -- I/O Signals
   signal slot_cyc:    slot_std_logic_type;
@@ -226,8 +224,6 @@ architecture behave of de0_nano_top is
   signal ramwbi:  wb_mosi_type;
   signal ramwbo:  wb_p_miso_type;
 
-  alias RXD: std_logic is GPIO_0(0);
-  alias TXD: std_logic is GPIO_0(1);
 
   alias SPI_MISO: std_logic is EPCS_DATA0;
   alias SPI_MOSI: std_logic is EPCS_ASDO;
@@ -251,19 +247,79 @@ architecture behave of de0_nano_top is
   signal sda2_padoen_o  : std_logic;                    -- i2c data line output enable, active low
 
 
-  alias RGB_R0: std_logic is GPIO_0(13);
-  alias RGB_G0: std_logic is GPIO_0(9);
-  alias RGB_B0: std_logic is GPIO_0(15);
-  alias RGB_R1: std_logic is GPIO_0(16);
-  alias RGB_G1: std_logic is GPIO_0(17);
-  alias RGB_B1: std_logic is GPIO_0(19);
-  alias RGB_A: std_logic is GPIO_0(18);
-  alias RGB_B: std_logic is GPIO_0(22);
-  alias RGB_C: std_logic is GPIO_0(23);
-  alias RGB_D: std_logic is GPIO_0(26);
-  alias RGB_CLK: std_logic is GPIO_0(27);
-  alias RGB_STB: std_logic is GPIO_0(28);
-  alias RGB_OE: std_logic is GPIO_0(29);
+
+  alias RXD:        std_logic is GPIO_0(0);
+  alias TXD:        std_logic is GPIO_0(1);
+  alias WING_C_0:   std_logic is GPIO_0(2);
+  alias WING_C_1:   std_logic is GPIO_0(3);
+  -- GPIO0(4) is unconnected on adaptor board
+  alias WING_C_3:   std_logic is GPIO_0(5);
+  alias WING_C_2:   std_logic is GPIO_0(6);
+  alias WING_C_5:   std_logic is GPIO_0(7);
+  alias WING_C_4:   std_logic is GPIO_0(8);
+  alias RGB_G0:     std_logic is GPIO_0(9);
+
+  alias WING_C_7:   std_logic is GPIO_0(10);
+  alias WING_C_6:   std_logic is GPIO_0(11);
+  alias WING_C_9:   std_logic is GPIO_0(12);
+  alias RGB_R0:     std_logic is GPIO_0(13);
+  alias WING_C_8:   std_logic is GPIO_0(14);
+  alias RGB_B0:     std_logic is GPIO_0(15);
+  alias RGB_R1:     std_logic is GPIO_0(16);
+  alias RGB_G1:     std_logic is GPIO_0(17);
+  alias RGB_A:      std_logic is GPIO_0(18);
+  alias RGB_B1:     std_logic is GPIO_0(19);
+
+  alias WING_C_10:  std_logic is GPIO_0(20);
+  alias WING_C_11:  std_logic is GPIO_0(21);
+  alias RGB_B:      std_logic is GPIO_0(22);
+  alias RGB_C:      std_logic is GPIO_0(23);
+  alias WING_C_12:  std_logic is GPIO_0(24);
+  alias WING_C_13:  std_logic is GPIO_0(25);
+  alias RGB_D:      std_logic is GPIO_0(26);
+  alias RGB_CLK:    std_logic is GPIO_0(27);
+  alias RGB_STB:    std_logic is GPIO_0(28);
+  alias RGB_OE:     std_logic is GPIO_0(29);
+  -- GPIO0(30) is unconnected on adaptor board
+  alias WING_C_14:  std_logic is GPIO_0(31);
+  alias WING_C_15:  std_logic is GPIO_0(32);
+  -- GPIO0(33) is unconnected on adaptor board
+
+  alias WING_B_0:   std_logic is GPIO_1(33);
+  alias WING_B_1:   std_logic is GPIO_1(31);
+  alias WING_B_2:   std_logic is GPIO_1(29);
+  alias WING_B_3:   std_logic is GPIO_1(27);
+  alias WING_B_4:   std_logic is GPIO_1(25);
+  alias WING_B_5:   std_logic is GPIO_1(23);
+  alias WING_B_6:   std_logic is GPIO_1(21);
+  alias WING_B_7:   std_logic is GPIO_1(19);
+  alias WING_B_8:   std_logic is GPIO_1(17);
+  alias WING_B_9:   std_logic is GPIO_1(15);
+  alias WING_B_10:  std_logic is GPIO_1(13);
+  alias WING_B_11:  std_logic is GPIO_1(11);
+  alias WING_B_12:  std_logic is GPIO_1(9);
+  alias WING_B_13:  std_logic is GPIO_1(7);
+  alias WING_B_14:  std_logic is GPIO_1(5);
+  alias WING_B_15:  std_logic is GPIO_1(3);
+
+  alias WING_A_0:   std_logic is GPIO_1(2);
+  alias WING_A_1:   std_logic is GPIO_1(4);
+  alias WING_A_2:   std_logic is GPIO_1(6);
+  alias WING_A_3:   std_logic is GPIO_1(8);
+  alias WING_A_4:   std_logic is GPIO_1(10);
+  alias WING_A_5:   std_logic is GPIO_1(12);
+  alias WING_A_6:   std_logic is GPIO_1(14);
+  alias WING_A_7:   std_logic is GPIO_1(16);
+  alias WING_A_8:   std_logic is GPIO_1(18);
+  alias WING_A_9:   std_logic is GPIO_1(20);
+  alias WING_A_10:  std_logic is GPIO_1(22);
+  alias WING_A_11:  std_logic is GPIO_1(24);
+  alias WING_A_12:  std_logic is GPIO_1(26);
+  alias WING_A_13:  std_logic is GPIO_1(28);
+  alias WING_A_14:  std_logic is GPIO_1(30);
+  alias WING_A_15:  std_logic is GPIO_1(32);
+
+
 
   signal clk16: std_logic;
 
@@ -311,23 +367,57 @@ begin
 
   -- first 2 GPIO reserved for UART
 
-  -- GPIO 2 to 33 map to GPIO0 block
 
-  --gp0_gen: for i in 0 to 31 generate
-  --  pin: IOPAD port map(I => gpio_o(i),O => gpio_i(i),T => gpio_t(i),C => sysclk,PAD => GPIO_0(i+2) );
-  --end generate;
+  wa00: IOPAD port map(I => gpio_o(0),O => gpio_i(0),T => gpio_t(0),C => sysclk,PAD => WING_A_0 );
+  wa01: IOPAD port map(I => gpio_o(1),O => gpio_i(1),T => gpio_t(1),C => sysclk,PAD => WING_A_1 );
+  wa02: IOPAD port map(I => gpio_o(2),O => gpio_i(2),T => gpio_t(2),C => sysclk,PAD => WING_A_2 );
+  wa03: IOPAD port map(I => gpio_o(3),O => gpio_i(3),T => gpio_t(3),C => sysclk,PAD => WING_A_3 );
+  wa04: IOPAD port map(I => gpio_o(4),O => gpio_i(4),T => gpio_t(4),C => sysclk,PAD => WING_A_4 );
+  wa05: IOPAD port map(I => gpio_o(5),O => gpio_i(5),T => gpio_t(5),C => sysclk,PAD => WING_A_5 );
+  wa06: IOPAD port map(I => gpio_o(6),O => gpio_i(6),T => gpio_t(6),C => sysclk,PAD => WING_A_6 );
+  wa07: IOPAD port map(I => gpio_o(7),O => gpio_i(7),T => gpio_t(7),C => sysclk,PAD => WING_A_7 );
+  wa08: IOPAD port map(I => gpio_o(8),O => gpio_i(8),T => gpio_t(8),C => sysclk,PAD => WING_A_8 );
+  wa09: IOPAD port map(I => gpio_o(9),O => gpio_i(9),T => gpio_t(9),C => sysclk,PAD => WING_A_9 );
+  wa10: IOPAD port map(I => gpio_o(10),O => gpio_i(10),T => gpio_t(10),C => sysclk,PAD => WING_A_10 );
+  wa11: IOPAD port map(I => gpio_o(11),O => gpio_i(11),T => gpio_t(11),C => sysclk,PAD => WING_A_11 );
+  wa12: IOPAD port map(I => gpio_o(12),O => gpio_i(12),T => gpio_t(12),C => sysclk,PAD => WING_A_12 );
+  wa13: IOPAD port map(I => gpio_o(13),O => gpio_i(13),T => gpio_t(13),C => sysclk,PAD => WING_A_13 );
+  wa14: IOPAD port map(I => gpio_o(14),O => gpio_i(14),T => gpio_t(14),C => sysclk,PAD => WING_A_14 );
+  wa15: IOPAD port map(I => gpio_o(15),O => gpio_i(15),T => gpio_t(15),C => sysclk,PAD => WING_A_15 );
 
-  -- GPIO 34 to 67 map to GPIO1 block
-  gp1_gen: for i in 0 to 33 generate
-    pin: IOPAD port map(I => gpio_o(32+i),O => gpio_i(32+i),T => gpio_t(32+i),C => sysclk,PAD => GPIO_1(i) );
-  end generate;
+  wb00: IOPAD port map(I => gpio_o(16),O => gpio_i(16),T => gpio_t(16),C => sysclk,PAD => WING_B_0 );
+  wb01: IOPAD port map(I => gpio_o(17),O => gpio_i(17),T => gpio_t(17),C => sysclk,PAD => WING_B_1 );
+  wb02: IOPAD port map(I => gpio_o(18),O => gpio_i(18),T => gpio_t(18),C => sysclk,PAD => WING_B_2 );
+  wb03: IOPAD port map(I => gpio_o(19),O => gpio_i(19),T => gpio_t(19),C => sysclk,PAD => WING_B_3 );
+  wb04: IOPAD port map(I => gpio_o(20),O => gpio_i(20),T => gpio_t(20),C => sysclk,PAD => WING_B_4 );
+  wb05: IOPAD port map(I => gpio_o(21),O => gpio_i(21),T => gpio_t(21),C => sysclk,PAD => WING_B_5 );
+  wb06: IOPAD port map(I => gpio_o(22),O => gpio_i(22),T => gpio_t(22),C => sysclk,PAD => WING_B_6 );
+  wb07: IOPAD port map(I => gpio_o(23),O => gpio_i(23),T => gpio_t(23),C => sysclk,PAD => WING_B_7 );
+  wb08: IOPAD port map(I => gpio_o(24),O => gpio_i(24),T => gpio_t(24),C => sysclk,PAD => WING_B_8 );
+  wb09: IOPAD port map(I => gpio_o(25),O => gpio_i(25),T => gpio_t(25),C => sysclk,PAD => WING_B_9 );
+  wb10: IOPAD port map(I => gpio_o(26),O => gpio_i(26),T => gpio_t(26),C => sysclk,PAD => WING_B_10 );
+  wb11: IOPAD port map(I => gpio_o(27),O => gpio_i(27),T => gpio_t(27),C => sysclk,PAD => WING_B_11 );
+  wb12: IOPAD port map(I => gpio_o(28),O => gpio_i(28),T => gpio_t(28),C => sysclk,PAD => WING_B_12 );
+  wb13: IOPAD port map(I => gpio_o(29),O => gpio_i(29),T => gpio_t(29),C => sysclk,PAD => WING_B_13 );
+  wb14: IOPAD port map(I => gpio_o(30),O => gpio_i(30),T => gpio_t(30),C => sysclk,PAD => WING_B_14 );
+  wb15: IOPAD port map(I => gpio_o(31),O => gpio_i(31),T => gpio_t(31),C => sysclk,PAD => WING_B_15 );
 
-
-  -- GPIO 68 to 78 map to GPIO2 block
-
-  gp2_gen: for i in 0 to 12 generate
-    pin: IOPAD port map(I => gpio_o(32+34+i),O => gpio_i(32+34+i),T => gpio_t(32+34+i),C => sysclk,PAD => GPIO_2(i) );
-  end generate;
+  wc00: IOPAD port map(I => gpio_o(32),O => gpio_i(32),T => gpio_t(32),C => sysclk,PAD => WING_C_0 );
+  wc01: IOPAD port map(I => gpio_o(33),O => gpio_i(33),T => gpio_t(33),C => sysclk,PAD => WING_C_1 );
+  wc02: IOPAD port map(I => gpio_o(34),O => gpio_i(34),T => gpio_t(34),C => sysclk,PAD => WING_C_2 );
+  wc03: IOPAD port map(I => gpio_o(35),O => gpio_i(35),T => gpio_t(35),C => sysclk,PAD => WING_C_3 );
+  wc04: IOPAD port map(I => gpio_o(36),O => gpio_i(36),T => gpio_t(36),C => sysclk,PAD => WING_C_4 );
+  wc05: IOPAD port map(I => gpio_o(37),O => gpio_i(37),T => gpio_t(37),C => sysclk,PAD => WING_C_5 );
+  wc06: IOPAD port map(I => gpio_o(38),O => gpio_i(38),T => gpio_t(38),C => sysclk,PAD => WING_C_6 );
+  wc07: IOPAD port map(I => gpio_o(39),O => gpio_i(39),T => gpio_t(39),C => sysclk,PAD => WING_C_7 );
+  wc08: IOPAD port map(I => gpio_o(40),O => gpio_i(40),T => gpio_t(40),C => sysclk,PAD => WING_C_8 );
+  wc09: IOPAD port map(I => gpio_o(41),O => gpio_i(41),T => gpio_t(41),C => sysclk,PAD => WING_C_9 );
+  wc10: IOPAD port map(I => gpio_o(42),O => gpio_i(42),T => gpio_t(42),C => sysclk,PAD => WING_C_10 );
+  wc11: IOPAD port map(I => gpio_o(43),O => gpio_i(43),T => gpio_t(43),C => sysclk,PAD => WING_C_11 );
+  wc12: IOPAD port map(I => gpio_o(44),O => gpio_i(44),T => gpio_t(44),C => sysclk,PAD => WING_C_12 );
+  wc13: IOPAD port map(I => gpio_o(45),O => gpio_i(45),T => gpio_t(45),C => sysclk,PAD => WING_C_13 );
+  wc14: IOPAD port map(I => gpio_o(46),O => gpio_i(46),T => gpio_t(46),C => sysclk,PAD => WING_C_14 );
+  wc15: IOPAD port map(I => gpio_o(47),O => gpio_i(47),T => gpio_t(47),C => sysclk,PAD => WING_C_15 );
 
   -- Other ports are special, we need to avoid outputs on input-only pins
 
--- zpu/hdl/zpuino/common/board_de0nano.h
+++ zpu/hdl/zpuino/common/board_de0nano.h
@@ -38,6 +38,56 @@
 #define FPGA_PIN_ACCEL_CS    94
 #define FPGA_PIN_ADC_CS      95
 
+#define WING_A_0 0
+#define WING_A_1 1
+#define WING_A_2 2
+#define WING_A_3 3
+#define WING_A_4 4
+#define WING_A_5 5
+#define WING_A_6 6
+#define WING_A_7 7
+#define WING_A_8 8
+#define WING_A_9 9
+#define WING_A_10 10
+#define WING_A_11 11
+#define WING_A_12 12
+#define WING_A_13 13
+#define WING_A_14 14
+#define WING_A_15 15
+
+#define WING_B_0 16
+#define WING_B_1 17
+#define WING_B_2 18
+#define WING_B_3 19
+#define WING_B_4 20
+#define WING_B_5 21
+#define WING_B_6 22
+#define WING_B_7 23
+#define WING_B_8 24
+#define WING_B_9 25
+#define WING_B_10 26
+#define WING_B_11 27
+#define WING_B_12 28
+#define WING_B_13 29
+#define WING_B_14 30
+#define WING_B_15 31
+        
+#define WING_C_0 32
+#define WING_C_1 33
+#define WING_C_2 34
+#define WING_C_3 35
+#define WING_C_4 36
+#define WING_C_5 37
+#define WING_C_6 38
+#define WING_C_7 39
+#define WING_C_8 40
+#define WING_C_9 41
+#define WING_C_10 42
+#define WING_C_11 43
+#define WING_C_12 44
+#define WING_C_13 45
+#define WING_C_14 46
+#define WING_C_15 47
 
 
 #define SPI_FLASH_SEL_PIN FPGA_PIN_FLASHCS
